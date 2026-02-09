3b6690b69d72f4158a66065973734281933fd338
tismajo <gir23559@uvg.edu.gt>
2026-02-08 18:34:55 -0600

    feat: Descripción general de los datos

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
 import pandas as pd
-import os
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-df = pd.read_spss()
+df = pd.read_csv("./data/collectedData.csv", encoding="utf-8")
+
+
+""" 
+DESCRIPCIÓN GENERAL DE LOS DATOS
+"""
+# Cantidad de observaiones y variables
+rows, cols = df.shape
+print(f"Cantidad de observaciones: {rows}")
+print(f"Cantidad de variables: {cols}")
+
+# Nombres de las variables
+varNames = df.columns.tolist()
+print(f"\nVariables del dataset: {varNames}")
+
+# Tipos de variables
+print("\nTipos de datos:")
+print(df.dtypes)
+
+# Valores faltantes
+print("\nValores faltantes por variable:")
+print(df.isna().sum().sort_values(ascending=False))
